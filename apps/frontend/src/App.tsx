6a4e892ef2e6f06c2942f4f34a4d694e09a0e0a5
Codechef555 <mdkaraamathullahsheriff@gmail.com>
2026-08-24 01:34:14 +0530

    added the imports for the file

--- apps/frontend/src/App.tsx
+++ apps/frontend/src/App.tsx
@@ -1,4 +1,6 @@
-
+import "styles/globals.css"
+import { Button } from "./components/ui/button";
+import { Input } from "./components/ui/input";
 export function App() {
   return (
     <div className="h-screen w-screen flex justify-center item-center">
