f74578f045f11e5d423536e5b44204c05f3fb9bc
Codechef555 <mdkaraamathullahsheriff@gmail.com>
2026-08-24 01:27:46 +0530

    Removed the unused imports

--- apps/frontend/src/App.tsx
+++ apps/frontend/src/App.tsx
@@ -1,10 +1,3 @@
-import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
-import { APITester } from "./APITester";
-import "./index.css";
-
-import logo from "./logo.svg";
-import reactLogo from "./react.svg";
-
 export function App() {
   return (
     <div className="container mx-auto p-8 text-center relative z-10">
