29f721756ef0294e03ea516943b6a5d45421bb46
Codechef555 <mdkaraamathullahsheriff@gmail.com>
2026-08-24 02:01:11 +0530

    added in the page

--- apps/frontend/src/App.tsx
+++ apps/frontend/src/App.tsx
@@ -3,12 +3,14 @@ import { Button } from "./components/ui/button";
 import { Input } from "./components/ui/input";
 import { Form } from "./components/Form";
 import { useState } from "react";
+import { Interview } from "./components/Interview";
 export function App() {
   const [page, setPage] = useState<"form" | "interview" | "result">("form");
   return (
     <div>
       {page == "form" && <Form />}
       {page == "interview" && <Interview />}
+
     </div>
 
   );
