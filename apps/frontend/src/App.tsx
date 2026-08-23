b26a02fd9a27d9f757d5425a2223efa73ee4a87d
Codechef555 <mdkaraamathullahsheriff@gmail.com>
2026-08-24 01:43:34 +0530

    added typography shadcn h2 tag

--- apps/frontend/src/App.tsx
+++ apps/frontend/src/App.tsx
@@ -5,6 +5,9 @@ export function App() {
   return (
     <div className="h-screen w-screen flex justify-center item-center">
       <div>
+        <h2 className="scroll-m-20 border-b pb-2 text-3xl font-semibold tracking-tight first:mt-0">
+          AI interview kickstart
+        </h2>
         <div className="p-4">
           <input placeholder="Linkedin URL" />
         </div>
