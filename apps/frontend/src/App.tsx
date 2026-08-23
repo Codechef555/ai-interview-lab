64cfc273fdade387c986c644bb9acc82533ce2b6
Codechef555 <mdkaraamathullahsheriff@gmail.com>
2026-08-24 01:33:05 +0530

    added tailwind css to the inputs and button

--- apps/frontend/src/App.tsx
+++ apps/frontend/src/App.tsx
@@ -1,5 +1,13 @@
+
 export function App() {
   return (
+    <div className="h-screen w-screen flex justify-center item-center">
+      <div>
+        <input />
+        <input />
+        <button>Start Interview</button>
+      </div>
+    </div>
   );
 }
 
