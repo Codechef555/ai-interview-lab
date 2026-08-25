a651c3b6497bd8b02ed56b96dbfe5f2d781e801f
Codechef555 <mdkaraamathullahsheriff@gmail.com>
2026-08-25 07:25:11 +0530

    added proxy credentials

--- apps/backend/scrapers/github.ts
+++ apps/backend/scrapers/github.ts
@@ -3,7 +3,17 @@ import axios from "axios";
 //const httpsAgent = new HttpsProxyAgent(process.env.PROXY_URL!);
 
 export async function scrapeGithub(username: string) {
-    const userRepos = await axios.get(`https://api.github.com/users/${username}/repos`);
+    const userRepos = await axios.get(`https://api.github.com/users/${username}/repos`, {
+        proxy: {
+            host: "gw.dataimpulse.com",
+            port: number,
+            auth: {
+                username: process.env.DATAIMPULSE_PROXY_USER!,
+                password: process.env.DATAIMPULSE_PROXY_PASSWORD!,
+            },
+            protocol: "http"
+        }
+    });
     return userRepos.data.map((x: any) => ({
         description: x.description,
         name: x.name,
