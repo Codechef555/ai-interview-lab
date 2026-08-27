5207cd4cfc48d3681a8b19eb8c3c8651b35452a0
Codechef555 <mdkaraamathullahsheriff@gmail.com>
2026-08-27 11:00:42 +0530

    added useEffect

--- apps/frontend/src/components/Interview.tsx
+++ apps/frontend/src/components/Interview.tsx
@@ -1,7 +1,12 @@
+import { useEffect } from "react";
 import { useParams } from "react-router"
 
 export function Interview() {
     const { InterviewId } = useParams();
+
+    useEffect(() => {
+
+    }, [])
     return <div>
         Interview
     </div>
