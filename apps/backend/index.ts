fe504ec5fe957077f1a197b403389700dfa7e823
Md.Karaamathullah Sheriff <90059832+Codechef555@users.noreply.github.com>
2026-08-30 20:46:57 +0530

    Update index.ts

--- apps/backend/index.ts
+++ apps/backend/index.ts
@@ -82,6 +82,7 @@ app.post("/api/v1/session/:interviewId", async (req, res) => {
     }
 });
 
+#registers the data based on the each and every unique id for slot
 app.post("/api/v1/session/user/response/:interviewId", async(req,res) => {
     const { message } = req.body;
     await prism.message.create({
