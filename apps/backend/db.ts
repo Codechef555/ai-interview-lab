3af6ae8de8a8240622be4ca995c5481c444818ca
Md.Karaamathullah Sheriff <90059832+Codechef555@users.noreply.github.com>
2026-08-31 23:26:19 +0530

    Update db.ts

--- apps/backend/db.ts
+++ apps/backend/db.ts
@@ -1,6 +1,7 @@
 import { PrismaClient } from "./generated/prisma/client";
 import { PrismaPg } from "@prisma/adapter-pg";
 
+// connect the db url with an external call
 const adapter = new PrismaPg({
     connectionString: process.env.DATABASE_URL!,
 });
