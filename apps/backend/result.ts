732aace05513f0425c4557abc6d5e731b1f057f6
Codechef555 <mdkaraamathullahsheriff@gmail.com>
2026-09-03 18:23:17 +0530

    added feedback description

--- apps/backend/result.ts
+++ apps/backend/result.ts
@@ -4,8 +4,8 @@ import { zodToJsonSchema } from "zod-to-json-schema"
 
 const ai = new GoogleGenAI({ apiKey: process.env.GEMINI_API_KEY })
 const outputSchema = z.object({
-    feedback:
-        score:
+    feedback: z.string().describe("Feedback for the User"),
+    score:
 })
 export const calculateResult(messages: { type: "Assistant" | "User", message: string, createdAt: Date }[]){
     const response = await ai.models.generateContent({
