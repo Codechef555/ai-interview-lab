be9265b1266ccfa0759c192d2109ea6b58fa7d46
Codechef555 <mdkaraamathullahsheriff@gmail.com>
2026-09-03 18:15:11 +0530

    added the response of the model information

--- apps/backend/result.ts
+++ apps/backend/result.ts
@@ -1,3 +1,12 @@
 export const calculateResult(messages: { type: "Assistant" | "User", message: string, createdAt: Date }[]){
-
+    const response = await ai.models.generateContent({
+        model: "gemini-3.5-flash",
+        contents: RESULT_PROMPT.replace(`{{USER_TRANSCRIPT}}`, JSON.stringify(messages)),
+        config: {
+            responseFormat: { text: { mimeType: "application/json", schema: zodToJsonSchema(outputSchema) } },
+        },
+    });
+    console.log(response.text!);
+    const result = outputSchema.parse(JSON.parse(response.text!));
+    return result;
 }
