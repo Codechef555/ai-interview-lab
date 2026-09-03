216dca3de7e937f700716843e324feac7fc8a95d
Codechef555 <mdkaraamathullahsheriff@gmail.com>
2026-09-03 18:19:32 +0530

    added google gemiini api key to this file

--- apps/backend/result.ts
+++ apps/backend/result.ts
@@ -1,7 +1,7 @@
 import { z } from "zod";
 import { GoogleGenAI } from "@google/genai";
 
-const ai = new G
+const ai = new GoogleGenAI({ process.env.GEMINI_API_KEY })
 export const calculateResult(messages: { type: "Assistant" | "User", message: string, createdAt: Date }[]){
     const response = await ai.models.generateContent({
         model: "gemini-3.5-flash",
