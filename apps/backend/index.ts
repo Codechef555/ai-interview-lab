9421296592448066f8924dfe97bbee3a780c4b74
Md.Karaamathullah Sheriff <90059832+Codechef555@users.noreply.github.com>
2026-09-09 15:43:22 +0530

    updated the changes

--- apps/backend/index.ts
+++ apps/backend/index.ts
@@ -113,16 +113,16 @@ app.get("/api/v1/result/:interviewId", async (req, res) => {
 //if the interview in progress this function should execute 
     if (interview.status === "Inprogress") {
         const result = await calculateResult(interview.conversations)
-            prisma.interview.update({
-                where: {
+        await prisma.interview.update({
+            where: {
                 id: req.params.interviewId;
-                },
-                data: {
-                    status: "Done",
-                    feedback: result.feedback
-                }
-            }) 
-            
+            },
+            data: {
+                status: "Done",
+                feedback: result.feedback
+            }
+        }) 
+
     }
 
 
