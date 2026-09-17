0f15121af232091df664471b5e7d267355be7d7b
Md.Karaamathullah Sheriff <90059832+Codechef555@users.noreply.github.com>
2026-09-17 22:36:53 +0530

    Update card.tsx

--- apps/frontend/src/components/ui/card.tsx
+++ apps/frontend/src/components/ui/card.tsx
@@ -1,7 +1,7 @@
 import * as React from "react";
 
 import { cn } from "@/lib/utils";
-
+// adding card components
 function Card({ className, ...props }: React.ComponentProps<"div">) {
   return (
     <div
